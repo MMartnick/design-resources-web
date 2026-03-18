b33b4bec26975b53cd6ce8f295f64e4b9dfcec2d
doom_monsta <mmartnick@proton.me>
2026-03-18 18:37:18 +0900

    First push

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repoName = process.env.NEXT_PUBLIC_BASE_PATH ?? "";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: repoName,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
